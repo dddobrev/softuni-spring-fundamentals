d262c0910feffca5ecd402a56beb8cc4541bbc90
luchob <lachezar.balev@gmail.com>
2021-01-28 18:19:25 +0200

    Adding an empty skeleton for lection 6.

---
+++ lection6/src/test/java/bg/softuni/lection6/Lection6ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.lection6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lection6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
