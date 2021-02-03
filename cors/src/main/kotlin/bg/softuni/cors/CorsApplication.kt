6ea3d536cc59474ad99017200288d2ecf7f2c4f1
luchob <lachezar.balev@gmail.com>
2021-02-03 19:08:42 +0200

    Sample CORS server

---
+++ cors/src/main/kotlin/bg/softuni/cors/CorsApplication.kt
@@ -0,0 +1,11 @@
+package bg.softuni.cors
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CorsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CorsApplication>(*args)
+}
---
+++ cors/src/main/kotlin/bg/softuni/cors/HelloMessage.kt
@@ -0,0 +1,6 @@
+package bg.softuni.cors
+
+data class HelloMessage(
+    val message: String,
+    val from: String
+)
